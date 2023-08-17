9fd9c4b6a00828e0a3d2aa6c7600ad10e56a8121
npv34 <phanvanluan611996@gmail.com>
2023-08-17 09:38:02 +0700

    first commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Contracts\Session\Session;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    function showFormLogin(){
+        return view('login');
+    }
+
+    function login(Request $request)  {
+        $email = $request->email;
+        $password = $request->password;
+        if($email == 'admin@gmail.com' && $password == '1234'){
+            // tạo session lưu email đăng nhập
+            \Illuminate\Support\Facades\Session::put('userEmail', $email);
+            return redirect()->route('home');
+        } else {
+            // session flash chỉ được tồn tại duy nhất trong một request
+            \Illuminate\Support\Facades\Session::flash('error', 'Account not exist');
+            return redirect()->route('auth.showFormLogin');
+        }
+    }
+
+    function logout(){
+        // xóa tất cả các session
+        session()->flush();
+        return redirect()->route('auth.showFormLogin');
+    }
+}
